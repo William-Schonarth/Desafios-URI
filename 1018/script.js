3cf30c67ddafce6af93150801df947cb26628075
William Schonarth <56305304+William-Schonarth@users.noreply.github.com>
2020-12-15 17:58:59 -0300

    Iniciado desafio 1087

--- 1018/script.js
+++ 1018/script.js
@@ -1,9 +1,15 @@
 var input = require('fs').readFileSync('stdin', 'utf8');
 var lines = input.split('\n');
 
-let horas = lines.shift();
-let velocidadeMedia = lines.shift();
+let valor = parseInt(lines.shift());
 
-let litros = ((horas * velocidadeMedia) / 12).toFixed(3);
+let notas = [100, 50, 20, 10, 5, 2, 1];
 
-console.log(litros);
+console.log(valor);
+
+notas.forEach(notas => {
+  let quantidadeNotas = Math.trunc(valor / notas);
+  console.log(`${quantidadeNotas} nota(s) de R$ ${notas + ',00'}`);
+  valor -= quantidadeNotas * notas;
+  console.log(valor)
+});
